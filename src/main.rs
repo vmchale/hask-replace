9a8d543e64482b4b66f75d601a0ec57c2249ec5d
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-07-28 21:57:33 -0500

    clippy lints

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,15 @@ struct ProjectOwned {
     pub cabal_file: PathBuf
 }
 
-fn find_by_end_vec(ref p: &PathBuf, find: &str, depth: Option<usize>) -> Vec<PathBuf> {
+impl ProjectOwned {
+   
+    fn get_cabal_path(&self) -> String {
+        get_cabal(&self.dir).cabal_file.to_string_lossy().to_string()
+    }
+}
+
+
+fn find_by_end_vec(p: &PathBuf, find: &str, depth: Option<usize>) -> Vec<PathBuf> {
 
     let s = p.to_string_lossy().to_string();
 
@@ -45,13 +53,12 @@ fn find_by_end_vec(ref p: &PathBuf, find: &str, depth: Option<usize>) -> Vec<Pat
 
 
 /// The arguments passed in here should *always* be strings pointing to directories
-fn get_cabal(p: PathBuf) -> ProjectOwned {
+fn get_cabal(p: &PathBuf) -> ProjectOwned {
 
-    let parent = &p.parent().unwrap_or(&p);
+    let parent = p.parent().unwrap_or(p);
     let s = p.to_string_lossy().to_string();
 
-    // FIXME finding cabal files shouldn't require recursion down arbitrarily many levels
-    let vec = find_by_end_vec(&p, ".cabal", Some(1));
+    let mut vec = find_by_end_vec(p, ".cabal", Some(1));
     let vec_len = vec.len();
 
     // if we find more than one cabal file, abort.
@@ -61,7 +68,8 @@ fn get_cabal(p: PathBuf) -> ProjectOwned {
     } else if vec_len == 0 {
         ProjectOwned { dir: parent.to_path_buf(), cabal_file: p.clone() }
     } else {
-        ProjectOwned { dir: PathBuf::from(s), cabal_file: { let mut cabal_path = p.clone() ; cabal_path.push("/test-nothing.cabal") ; cabal_path } } // FIXME wrong path to cabal file.
+        let cabal_name = vec.pop().unwrap();
+        ProjectOwned { dir: PathBuf::from(s), cabal_file: { cabal_name } }
     }
 
 }
@@ -74,17 +82,17 @@ pub fn get_dir(paths_from_cli: Option<&str>) -> &str {
     }
 }
 
-fn get_directory_contents(p: PathBuf) -> Vec<PathBuf> {
+fn get_source_files(p: &PathBuf) -> Vec<PathBuf> {
 
     let s = p.to_string_lossy().to_string();
 
     let dir = WalkDir::new(s).into_iter();
 
     let filtered = dir.filter_map(|e| e.ok()).filter(|p| {
-        p.path().extension() == Some(OsStr::new("hs")) // TODO
+        p.path().extension() == Some(OsStr::new("hs"))
     });
 
-    filtered.map(|p| p.path().to_path_buf()).collect() // FIXME get rid of to_path_buf
+    filtered.map(|p| p.path().to_path_buf()).collect()
 
 }
 
@@ -94,8 +102,8 @@ fn module_to_file_name(module: &str) -> String {
     replacements
 }
 
-fn rayon_directory_contents(cabal: ProjectOwned, old_module: &str, new_module: &str) -> () {
-    let dir: Vec<PathBuf> = get_directory_contents(cabal.dir);
+fn rayon_directory_contents(cabal: &ProjectOwned, old_module: &str, new_module: &str) -> () {
+    let dir: Vec<PathBuf> = get_source_files(&cabal.dir);
     let iter = dir.into_par_iter().filter(|p| p.to_string_lossy().to_string().ends_with(".hs"));
     iter.for_each(|p| {
         let mut source_file = File::open(&p).unwrap();
@@ -111,7 +119,7 @@ fn replace_all(cabal: ProjectOwned, old_module: &str, new_module: &str) -> () {
 
     // step 1: determine that the module we want to replace in fact exists
     let mut old_module_vec = find_by_end_vec(&cabal.dir, &module_to_file_name(old_module), None);
-    let old_module_exists = !(old_module_vec.len() == 0);
+    let old_module_exists = !(old_module_vec.is_empty());
 
     let (old_module_name, src_dir) = if old_module_exists {
         let name = old_module_vec.pop().unwrap();
@@ -127,12 +135,20 @@ fn replace_all(cabal: ProjectOwned, old_module: &str, new_module: &str) -> () {
     };
 
     // TODO make this a method
-    let mut cabal_string = cabal.dir.to_string_lossy().to_string();
-    cabal_string.push_str(&cabal.cabal_file.to_string_lossy().to_string());
+    let cabal_string = cabal.get_cabal_path();
 
-    let mut cabal_file = File::open(&cabal_string).unwrap();
+    let mut cabal_file =
+        match File::open(&cabal_string) {
+            Ok(x) => x,
+            _ => { eprintln!("{}: Failed to open file at: {}", "Error".red(), cabal_string);
+                   exit(0x0001) },
+        };
     let mut contents = String::new();
-    cabal_file.read_to_string(&mut contents).unwrap();
+    match cabal_file.read_to_string(&mut contents) {
+        Ok(_) => (),
+        _ => { eprintln!("{}: Failed to read file at: {}", "Error".red(), contents);
+               exit(0x0001) },
+    }
 
     let in_cabal_file = (&contents).contains(old_module);
 
@@ -143,26 +159,41 @@ fn replace_all(cabal: ProjectOwned, old_module: &str, new_module: &str) -> () {
 
     // step 2: determine the targeted directory in fact exists, or make it ourselves.
     let vref: String = new_module.replace(".", "/");
-    let mut v: Vec<&str> = vref.split('/').collect();
-    v.pop();
-    let target_directory = v.join("");
+    let v: Vec<&str> = vref.split('/').collect();
+    let mut target_directory: String = src_dir.clone();
+    target_directory.push_str(&v.join("/"));
     if !PathBuf::from(&target_directory).exists() {
-        let _ = fs::create_dir_all(&target_directory);
+        match fs::create_dir_all(&target_directory) {
+            Ok (x) => x,
+            _ => { eprintln!("{}: failed to create directory '{}'", "Error".red(), target_directory) ;
+                   exit(0x0001) },
+        }
     }
 
     // step 3: replace the module in the '.cabal' file
-    let v: Vec<PathBuf> = vec![ PathBuf::from(&cabal_string) ];
-    let _: Vec<()> = v.into_iter().map(|p| {
-        let mut source_file = OpenOptions::new().read(true).open(&p).unwrap();
-        let mut source = String::new();
-        source_file.read_to_string(&mut source).unwrap();
-        let replacements = source.replace(old_module, new_module);
-        let mut source_file_write = OpenOptions::new().write(true).open(&p).unwrap();
-        let _ = source_file_write.write(replacements.as_bytes()).unwrap();
-    }).collect();
+    let p: PathBuf = PathBuf::from(&cabal_string);
+    let mut source_file =
+        match OpenOptions::new().read(true).open(&p) {
+            Ok(x) => x,
+            _ => { eprintln!("{}: Failed to open file at: {}", "Error".red(), p.display());
+                   exit(0x0001) },
+        };
+    let mut source = String::new();
+    match source_file.read_to_string(&mut source) {
+        Ok(_) => (),
+        _ => { eprintln!("{}: Failed to read file at: {}", "Error".red(), p.display());
+               exit(0x0001) },
+    }
+    let replacements = source.replace(old_module, new_module);
+    let mut source_file_write = OpenOptions::new().write(true).open(&p).unwrap();
+    match source_file_write.write(replacements.as_bytes()) {
+        Ok(_) => (),
+        _ => { eprintln!("{}: Failed to write file at: {}", "Error".red(), p.display());
+               exit(0x0001) },
+    }
 
     // step 4: replace every 'import Module' with 'import NewModule'
-    rayon_directory_contents(cabal, old_module, new_module);
+    rayon_directory_contents(&cabal, old_module, new_module);
 
     // step 5: move the actual file
     let mut new_module_path = src_dir;
@@ -197,7 +228,7 @@ fn main() {
 
         let new_module = command.value_of("new").unwrap(); // okay beacause a subcommand is required
 
-        let cabal_project = get_cabal(dir);
+        let cabal_project = get_cabal(&dir);
 
         replace_all(cabal_project, old_module, new_module);
 
