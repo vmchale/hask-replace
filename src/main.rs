923f9182c7452c3b1850c83c0bab95c905589f14
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-07-29 00:44:53 -0500

    for release

--- src/main.rs
+++ src/main.rs
@@ -128,9 +128,9 @@ fn rayon_directory_contents(cabal: &ProjectOwned, old_module: &str, new_module: 
         old_module_regex.push_str(&old_module.replace(".", "\\."));
         old_module_regex.push_str(")+");
         let mut old_module_regex = old_module.to_string();
-        old_module_regex.push_str("(\n|\\(|( *) \\(|( *) where)+?"); // TODO regex for cabal files
+        old_module_regex.push_str("(\n|\\(|( *) \\(|( *) where|\\.)+?"); // TODO regex for cabal files
         let re = Regex::new(&old_module_regex).unwrap();
-        let replacements = re.replacen(&source, 2, |caps: &Captures| {
+        let replacements = re.replacen(&source, 0, |caps: &Captures| {
             format!("{}{}", new_module, &caps[1])
         }).to_string();
         let mut source_file_write = File::create(&p).unwrap();
