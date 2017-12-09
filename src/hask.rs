f2d7538db2404d0af83c2c778a3c6b087f4ee683
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-12-08 19:40:19 -0600

    tiny steps

--- src/hask.rs
+++ src/hask.rs
@@ -180,7 +180,7 @@ named_args!(interesting_line<'a>(old: &'a str, old_dot: &'a str, new: &'a str, n
       do_parse!(a: tag!(old_dot) >> (swap_module(old_dot, new_dot, a))) |
       is_not!(special) |
       is_not!(" \n-{\"'") |
-      recognize!(do_parse!(alt!(tag!("' ") | tag!("'\n") | tag!("']") | tag!("'t") | tag!("''") | tag!("'''") | tag!("')") | tag!("',")) >> is_not!("'{") >> (()))) |
+      recognize!(do_parse!(alt!(tag!("' ") | tag!("'\n") | tag!("']") | tag!("'t") | tag!("''") | tag!("'''") | tag!("')") | tag!("',")) >> is_not!("'{\"") >> (()))) |
       char_contents |
       string_contents |
       skip |
